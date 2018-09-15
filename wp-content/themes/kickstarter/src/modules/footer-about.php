f274a8c718b59c6144f1f2a93ba27537c61a9ac8
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-09-15 05:31:39 +0100

    copyright and footer css and html

--- wp-content/themes/kickstarter/src/modules/footer-about.php
+++ wp-content/themes/kickstarter/src/modules/footer-about.php
@@ -5,9 +5,10 @@
         <img data-src="<?php echo wpimage('img=' . (int) get_field('nimble_footer_logo', 'options') . '&h=50&w=9999&crop=false&retina=true') ?>" src="<?php echo wpimage('img=' . (int) get_field('nimble_footer_logo', 'options') . '&h=50&w=9999&crop=false&retina=false') ?>" alt="<?php _e('Nimble Media logo', 'TEXT_DOMAIN');?>" class="lazy no">
       </a>
     </div>
-    <div class="content">
+    <div class="content" data-mh="fc">
       <?php the_field('nimble_media_text', 'options')?>
     </div>
+    <a href="<?php esc_url('http://www.womeninrail.org')?>" title="<?php _e('More about event organiser', 'TEXT_DOMAIN');?>" class="button outlined white uc"><?php _e('More about event organiser', 'TEXT_DOMAIN'); ?></a>
   </div>
   <div class="about-wir about">
     <div class="logo">
@@ -15,8 +16,9 @@
         <img data-src="<?php echo wpimage('img=' . (int) get_field('wir_footer_logo', 'options') . '&h=50&w=9999&crop=false&retina=true') ?>" src="<?php echo wpimage('img=' . (int) get_field('wir_footer_logo', 'options') . '&h=50&w=9999&crop=false&retina=false') ?>" alt="<?php _e('Women in Rail logo', 'TEXT_DOMAIN');?>" class="lazy no">
       </a>
     </div>
-    <div class="content">
+    <div class="content" data-mh="fc">
       <?php echo the_field('wir_text', 'options') ?>
     </div>
+    <a href="<?php esc_url('http://www.womeninrail.org')?>" title="<?php _e('Visit Women in Rail websit', 'TEXT_DOMAIN');?>" class="button outlined white uc"><?php _e('Visit website', 'TEXT_DOMAIN'); ?></a>
   </div>
 </div>
--- wp-content/themes/kickstarter/src/modules/footer-copyright.php
+++ wp-content/themes/kickstarter/src/modules/footer-copyright.php
@@ -1,9 +1,12 @@
 <div id="copyright">
-  <div class="container">
+  <div class="container first-last flex-between">
     <div class="left">
       <p><?php _e('Copyright', 'TEXT_DOMAIN');?> &copy; <?php echo get_the_date('Y') ?>
         <?php echo get_bloginfo('name') ?> | <a href="/terms-and-conditions/" ><?php _e('Terms and Conditions', 'TEXT_DOMAIN');?></a> |
         <a href="/privacy-statement/" ><?php _e('Privacy Statement & Cookie Notice', 'TEXT_DOMAIN');?></a> </p>
+    </div>
+    <div class="right">
+      <p><?php _e('Website delivered by', 'TEXT_DOMAIN'); ?> <a href="http://www.nimblemedia.co.uk"><?php _e('Nimble Media', 'TEXT_DOMAIN');?></a></p>
     </div>
   </div>
 </div>
