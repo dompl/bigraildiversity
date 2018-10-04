7d0f1a911a58a7b805eeceacf0de455ea4b574d9
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-10-04 07:35:09 +0100

    various fixes

--- wp-content/themes/kickstarter/src/footer.php
+++ wp-content/themes/kickstarter/src/footer.php
@@ -1,10 +1,12 @@
  </div>
+ <div id="footer-container">
  <div class="bar"></div>
  <footer id="footer">
   <div class="container first-last">
     <?php get_template_part('modules/footer', 'twitter');?>
     <?php get_template_part('modules/footer', 'about');?>
   </footer>
+  </div>
   <?php get_template_part('modules/footer', 'copyright');?>
    <div class="bar"></div>
   <?php wp_footer();?>
