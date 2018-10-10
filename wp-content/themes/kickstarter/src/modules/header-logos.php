fcc72a500ca89206f182537d4f2e437a0cd84330
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-10-10 10:29:02 +0100

    variuos changes

--- wp-content/themes/kickstarter/src/modules/header-logos.php
+++ wp-content/themes/kickstarter/src/modules/header-logos.php
@@ -13,28 +13,28 @@ $logo_size = 80;
         </a>
       </div>
     <?php endif?>
-    <?php if ($logo_2):
 
-      $logo = is_front_page() ? 47 : 49;
+  </div>
+  <div class="right flex-center">
+    <?php if ($logo_2):
+      $logo  = is_front_page() ? 47 : 49;
       $width = is_front_page() ? 154 : 69;
       ?>
       <div class="logo-2 logo">
         <a href="<?php echo esc_url(get_bloginfo('url')); ?>">
-          <img src="<?php echo wpimage('img=' . $logo . '&w='.$width.'&crop=false') ?>" data-src="<?php echo wpimage('img=' . $logo . '&w='.$width.'&retina=true&crop=false') ?>"  class="lazy no" alt="<?php echo bloginfo('name') ?> <?php _e('logo', 'TEXT_DOMAIN');?>">
+          <img src="<?php echo wpimage('img=' . $logo . '&w=' . $width . '&crop=false') ?>" data-src="<?php echo wpimage('img=' . $logo . '&w=' . $width . '&retina=true&crop=false') ?>"  class="lazy no" alt="<?php echo bloginfo('name') ?> <?php _e('logo', 'TEXT_DOMAIN');?>">
         </a>
       </div>
     <?php endif?>
-  </div>
-  <div class="right flex-center">
-    <?php if ($logo_3): ?>
-      <div class="logo-3 logo">
+    <?php /* if ($logo_3): ?>
+        <div class="logo-3 logo">
         <a href="http://www.nimblemedia.co.uk" title="<?php _e('Event organiser', 'TEXT_DOMAIN');?>" target="_blank">
-          <img src="<?php echo wpimage('img=' . $logo_3 . '&w=139&crop=false') ?>" data-src="<?php echo wpimage('img=' . $logo_3 . '&w=139&retina=true&crop=false') ?>" class="lazy no" alt="<?php echo bloginfo('name') ?> <?php _e('logo', 'TEXT_DOMAIN');?>">
+        <img src="<?php echo wpimage('img=' . $logo_3 . '&w=139&crop=false') ?>" data-src="<?php echo wpimage('img=' . $logo_3 . '&w=139&retina=true&crop=false') ?>" class="lazy no" alt="<?php echo bloginfo('name') ?> <?php _e('logo', 'TEXT_DOMAIN');?>">
         </a>
-      </div>
+        </div>
+      <?php endif */?>
       <div class="button">
-        <a href="<?php echo esc_url(get_permalink( 5 )); ?>" class="button medium fill color-wir" title="<?php _e('Attend Big Rail Diversity Challenge')?>"><?php _e('Enter', 'TEXT_DOMAIN');?> <span> <?php _e('Today', 'TEXT_DOMAIN');?></span></a>
+        <a href="<?php echo esc_url(get_permalink(5)); ?>" class="button medium fill color-green" title="<?php _e('Attend Big Rail Diversity Challenge')?>"><?php _e('Enter', 'TEXT_DOMAIN');?> <span> <?php _e('Today', 'TEXT_DOMAIN');?></span></a>
       </div>
-    <?php endif?>
+    </div>
   </div>
-</div>
