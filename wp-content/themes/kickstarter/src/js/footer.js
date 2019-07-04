9e7e5469700f9463ea5eda06510a54e0d85265c1
Dom Kapelewski <info@redfrogstudio.co.uk>
2019-07-04 08:52:53 +0100

    added new scores table

--- wp-content/themes/kickstarter/src/js/footer.js
+++ wp-content/themes/kickstarter/src/js/footer.js
@@ -8,9 +8,16 @@
 			//=require includes/modaal.js
 			//=require includes/faqs.js
 		}
+		function new_winners_score() {
+			$('.more-button .button').on('click' , function(){
+				$(this).parent('li').hide();
+				$('li.hidden').removeClass('hidden');
+			});
+		}
 
 		/* Initaite plugins */
 		initPlugins();
+		new_winners_score();
 
 		// Initiate plugins for pJax
 		if (typeof is_ks_pjax !== 'undefined') {
@@ -23,6 +30,7 @@
 			$(document).on('pjax:complete', function () {
 
 				initPlugins();
+				new_winners_score();
 
 			});
 		}
