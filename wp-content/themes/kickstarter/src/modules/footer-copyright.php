1164fe97096bb11dcbf063443200702d2c43afdc
Dom Kapelewski <info@redfrogstudio.co.uk>
2019-11-08 11:47:07 +0000

    removed terms link

--- wp-content/themes/kickstarter/src/modules/footer-copyright.php
+++ wp-content/themes/kickstarter/src/modules/footer-copyright.php
@@ -2,8 +2,7 @@
   <div class="container first-last flex-between">
     <div class="left">
       <p><?php _e('Copyright', 'TEXT_DOMAIN');?> &copy; <?php echo date('Y') ?>
-        <?php echo get_bloginfo('name') ?> | <a href="/terms-and-conditions/" ><?php _e('Terms and Conditions', 'TEXT_DOMAIN');?></a> |
-        <a href="/privacy-statement/" ><?php _e('Privacy Statement & Cookie Notice', 'TEXT_DOMAIN');?></a> </p>
+        <?php echo get_bloginfo('name') ?> | <a href="/privacy-statement/" ><?php _e('Privacy Statement & Cookie Notice', 'TEXT_DOMAIN');?></a> </p>
     </div>
     <div class="right">
       <p><?php _e('Website delivered by', 'TEXT_DOMAIN'); ?> <a href="http://www.nimblemedia.co.uk"><?php _e('Nimble Media', 'TEXT_DOMAIN');?></a></p>
